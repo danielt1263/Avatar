f8e94b3817cd7136c195b56f467c567ca32cf30c
Daniel Tartaglia <danielt1263@gmail.com>
2017-01-28 09:54:18 -0500

    Add action sheet function.

--- Avatar/UIViewControllerExtensions.swift
+++ Avatar/UIViewControllerExtensions.swift
@@ -11,6 +11,28 @@ import UIKit
 
 extension UIViewController
 {
+	func choiceIndexUsingActionSheet(title: String, message: String, choices: [String], onSourceView view: UIView) -> Promise<Int> {
+		return Promise(queue: DispatchQueue.main) { fulfill, reject in
+			let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+
+			let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+			let actions = choices.enumerated().map { offset, element in
+				UIAlertAction(title: element, style: .default, handler: { _ in fulfill(offset) })
+			}
+			
+			for action in actions + [cancelAction] {
+				alert.addAction(action)
+			}
+			
+			if let popoverPresentationController = alert.popoverPresentationController {
+				popoverPresentationController.sourceView = view
+				popoverPresentationController.sourceRect = view.bounds
+			}
+			self.present(alert, animated: true, completion: nil)
+		}
+	}
+	
 	func displayInformationAlert(title: String, message: String) -> Promise<Void> {
 		return Promise(queue: DispatchQueue.main) { fulfill, reject in
 			let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
