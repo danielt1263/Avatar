cec7768b994ef7630af23fc165a7c0a440470a36
Daniel Tartaglia <dtartaglia@hanekedesign.com>
2017-04-12 17:25:48 -0400

    Simplify error checking code.

--- Avatar/ViewController.swift
+++ Avatar/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController
 			self.avatarView.image = image
 		}
 		.catch { error in
-			guard error is UserInteractionError == false || (error as! UserInteractionError) != .userCanceled else { return }
+			guard error as? UserInteractionError != .userCanceled else { return }
 			self.displayInformationAlert(title: "Error", message: error.localizedDescription)
 		}
 	}
